7ccd7056d186ebbc9c82d690014d90f33aed9a66
Edward Contreras <matyasgoman@gmail.com>
2022-10-14 03:47:16 -0300

    Interfaces

--- pages/Pregunta 2.py
+++ pages/Pregunta 2.py
@@ -20,41 +20,64 @@ import pygame as py
 from pygame.locals import *
 import sys
 from PIL import Image 
+from PIL import ImageOps
 
-#Abriremos las imagenes y luego les cambiamos el tamaño
-def RotIm(fig,nom):
-    fig = Image.open(fig).resize((116,116))
-    fig.save(nom+".png")
-RotIm("Figura1.png","Fig1_py")
-RotIm("Figura2.png","Fig2_py")
-RotIm("Figura3.png","Fig3_py")
-RotIm("Figura4.png","Fig4_py")
+#Abrimos la imagen para cortar las figuras
+img = Image.open('figuras.png')
+#Generamos los border a cortar(izq,arriba,der,abajo)
+border1 = (4,6,766,5)
+#Se corta la imagen
+img1= ImageOps.crop(img,border1)
+#Se guarda bajo el nombre "Fig1.png"
+img1.save("Fig1.png")
+
+
+#Se repite el proceso con todas las figuras
+border2 = (242,5,503,20)
+img2= ImageOps.crop(img,border2)
+img2.save("Fig2.png")
+
+
+border3 = (504,23,264,30)
+img3 = ImageOps.crop(img,border3)
+img3.save("Fig3.png")
+
+
+border4 = (733,20,8,26)
+img4 = ImageOps.crop(img,border4)
+img4.save("Fig4.png")
+
+#Se reajusta el tamaño de la imagen
+img1.resize((116,116)).save("Fig1_r.png")
+img2.resize((116,116)).save("Fig2_r.png")
+img3.resize((116,116)).save("Fig3_r.png")
+img4.resize((116,116)).save("Fig4_r.png")
 
 #Iniciamos pygame
 py.init()
 #Seteamos el tamano de la pantalla 
 Screen= py.display.set_mode((759,185))
-#Cargamos imagenes y separa canales pero mantiene los canales alpha
-fig_1=py.image.load("Fig1_py.png")
-fig_2=py.image.load("Fig2_py.png")
-fig_3=py.image.load("Fig3_py.png")
-fig_4=py.image.load("Fig4_py.png")
+#Cargamos imagenes y se utiliza convert_alpha() para mantener la transparencia detras de la imagen
+fig_1=py.image.load("Fig1_r.png").convert_alpha()
+fig_2=py.image.load("Fig2_r.png").convert_alpha()
+fig_3=py.image.load("Fig3_r.png").convert_alpha()
+fig_4=py.image.load("Fig4_r.png").convert_alpha()
 
 #Cargamos la plantilla en la pantalla
 background=py.image.load("plantilla.png")
 
 #Rotamos las imagenes los grados que salen en la segunda variable
 rotar_1=py.transform.rotate(fig_1,80)
-rotar_2=py.transform.rotate(fig_2,100)
-rotar_3=py.transform.rotate(fig_3,50)
+rotar_2=py.transform.rotate(fig_2,99)
+rotar_3=py.transform.rotate(fig_3,51)
 rotar_4=py.transform.rotate(fig_4,112)
 
 #Cargamos todas las imagenes en sus respectivas posiciones
 Screen.blit(background,(0,0))
-Screen.blit(rotar_1,(10,20))
-Screen.blit(rotar_2,(204,18))
-Screen.blit(rotar_3,(380,12))
-Screen.blit(rotar_4,(600,13))
+Screen.blit(rotar_1,(9,19))
+Screen.blit(rotar_2,(202,20))
+Screen.blit(rotar_3,(379,12))
+Screen.blit(rotar_4,(599,13))
 
 py.display.flip()
 while True:
